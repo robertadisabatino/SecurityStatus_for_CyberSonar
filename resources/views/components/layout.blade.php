6c421d7cd1ebe1be3691d08eaab0e211d1e54c81
Roberta Di Sabatino <robertadisabatino@icloud.com>
2025-07-21 20:52:14 +0200

    name updated

--- resources/views/components/layout.blade.php
+++ resources/views/components/layout.blade.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Document</title>
+    <title>Security Status Cybersonar</title>
     @googlefonts
     <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
     @vite(['resources/css/app.css', 'resources/js/app.js'])
